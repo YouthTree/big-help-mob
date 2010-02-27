bc3642c4d68b67fa5aee237653d19f14098979fc
Matt Didcoe <matt@didcoe.id.au>
2010-02-27 12:39:05 +0800

    some styling so we aren't staring at a completely dull screen - this will change when we meet the designers from Youth Tree I imagine, but it's enough as a temporary measure

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Bighelpmob::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#show"
 
   # See how all your routes lay out with "rake routes"
 
