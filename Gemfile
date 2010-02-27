8ca805561148f92372ba78ca9aa9c976af19be45
Matt Didcoe <matt@didcoe.id.au>
2010-02-27 08:39:51 +0800

    haml'ing the app, updated Gemfile with a few new reqs - you'll need to install hominid (for Mailchimp)

--- Gemfile
+++ Gemfile
@@ -1,22 +1,17 @@
-# Edit this Gemfile to bundle your application's dependencies.
 source 'http://gemcutter.org'
-
 
 gem "rails", "3.0.0.beta"
 
-## Bundle edge rails:
-# gem "rails", :git => "git://github.com/rails/rails.git"
-
+gem "haml"
 gem "mysql"
 
-## Bundle the gems you use:
-# gem "bj"
-# gem "hpricot", "0.6"
-# gem "sqlite3-ruby", :require => "sqlite3"
-# gem "aws-s3", :require => "aws/s3"
+gem "hominid", :git => "git://github.com/bgetting/hominid.git"
 
-## Bundle gems used only in certain environments:
-# gem "rspec", :group => :test
-# group :test do
-#   gem "webrat"
-# end
+gem "formtastic"
+gem "rails3-generators"
+gem "rpx_now"
+
+group :test do
+  gem "shoulda"
+  gem "factory_girl"
+end
--- config/application.rb
+++ config/application.rb
@@ -30,13 +30,15 @@ module Bighelpmob
     # config.i18n.default_locale = :de
 
     # Configure generators values. Many other options are available, be sure to check the documentation.
-    # config.generators do |g|
-    #   g.orm             :active_record
-    #   g.template_engine :erb
-    #   g.test_framework  :test_unit, :fixture => true
-    # end
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :haml
+      g.test_framework  :test_unit, :fixture_replacement => :factory_girl
+    end
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters << :password
+    config.filter_parameters << :password_confirmation
+    
   end
 end
