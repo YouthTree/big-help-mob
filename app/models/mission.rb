9111595512535e5f094eb722b5c57f8dbcab9b72
Darcy Laycock <sutto@sutto.net>
2010-03-02 21:38:43 +0800

    misc refactoring

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -15,8 +15,7 @@ class Mission < ActiveRecord::Base
   def geocode_address
     return unless address.changed?
     geo = Geokit::Geocoders::MultiGeocoder.geocode(address.address_parts.join(", "))
-    #errors.add(:address, "Could not Geocode address") if !geo.success     NB: For now, don't fail if it can't geocode, we just won't include a map :-)
-    self.lat, self.lng = geo.lat,geo.lng if geo.success
+    self.lat, self.lng = geo.lat, geo.lng if geo.success
   end
 
   # Model methods
