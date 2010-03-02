0cc4115bd762363100f29f63c2ba85f0b6652420
Darcy Laycock <sutto@sutto.net>
2010-03-02 21:31:57 +0800

    Make tests run again

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,23 +1,24 @@
 class Mission < ActiveRecord::Base
   
   # Validations
-    validates_presence_of :name, :on => :create, :message => "can't be blank"
+  validates_presence_of :name, :on => :create, :message => "can't be blank"
 
   # Associations
   
   # Geocoding
   
-    acts_as_mappable
-    apply_addresslogic
-  
-    before_validation_on_create :geocode_address
+  acts_as_mappable
+  apply_addresslogic
 
-    def geocode_address
-      geo=Geokit::Geocoders::MultiGeocoder.geocode(address.address_parts.join(", "))
-      #errors.add(:address, "Could not Geocode address") if !geo.success     NB: For now, don't fail if it can't geocode, we just won't include a map :-)
-      self.lat, self.lng = geo.lat,geo.lng if geo.success
-    end
-  
+  before_save :geocode_address
+
+  def geocode_address
+    return unless address.changed?
+    geo = Geokit::Geocoders::MultiGeocoder.geocode(address.address_parts.join(", "))
+    #errors.add(:address, "Could not Geocode address") if !geo.success     NB: For now, don't fail if it can't geocode, we just won't include a map :-)
+    self.lat, self.lng = geo.lat,geo.lng if geo.success
+  end
+
   # Model methods
 
 end
