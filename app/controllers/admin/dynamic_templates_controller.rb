f21721bf1d14c25215ddd361cd626d1c625dc282
Darcy Laycock <sutto@sutto.net>
2010-04-21 00:11:37 +0800

    Fix controller issues

--- app/controllers/admin/dynamic_templates_controller.rb
+++ app/controllers/admin/dynamic_templates_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DynamicTemplatesController < Admin::BaseController
   
-  belongs_to :mission, :polymorphic => true
+  belongs_to :mission, :polymorphic => true, :finder => :find_sluggy
   
 end
