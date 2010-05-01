0b1ce3f05eecf2b40c47ec7737cf13f52500ae9f
Darcy Laycock <sutto@sutto.net>
2010-05-02 05:10:56 +0800

    Misc tweaks

--- app/workers/individual_user_mailer.rb
+++ app/workers/individual_user_mailer.rb
@@ -2,13 +2,9 @@
 class IndividualUserMailer < BaseUserMailer
   
   def process_email(email)
-    total         = email.user_count + 2
-    offset        = 2
+    total         = email.user_count + 1
+    offset        = 1
     at 0, total, "Preparing templates"
-    text_template    = email.template_for_text_content
-    html_template    = email.template_for_html_content
-    subject_template = email.template_for_subject
-    at 1, total, "Preparing templates"
     email.each_user do |user|
       send_mail! email.rendered_email_for(user), user.email
       at offset, total, "Sending email to #{user.email}"
