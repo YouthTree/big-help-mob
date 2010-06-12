d3e65a5d43427222ed57b698a8ad23147359997c
Darcy Laycock <sutto@sutto.net>
2010-06-12 11:36:59 +0800

    More misc updates, add some content to the missions archive page

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,17 +1,17 @@
 class Content < ActiveRecord::Base
-  
+
   attr_accessible :content, :key, :title, :type
-  
+
   validates_presence_of :key
-  
+
   def self.[](key)
     where(:key => key.to_s).first
   end
-  
+
   def content_as_html
     content.to_s.html_safe
   end
-  
+
 end
 
 # == Schema Info
