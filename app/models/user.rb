72e96ab0929eb473b399ebd3ee13a6d8f3ce1f3d
Darcy Laycock <sutto@sutto.net>
2010-09-12 18:42:06 +0800

    Add volunteered_in_last_year field to the database.

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
 
   attr_accessible :login, :password, :password_confirmation, :email, :display_name, :first_name,
                   :last_name, :date_of_birth, :phone, :postcode, :allergies, :mailing_list_choices,
-                  :captain_application_attributes, :origin, :date_of_birth
+                  :captain_application_attributes, :origin, :date_of_birth, :volunteered_in_last_year
 
   has_many :mission_participations, :dependent => :destroy
   
