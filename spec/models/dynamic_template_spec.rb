a977abb6ba9355fd1646fe25dfbc1d2438b8468e
Darcy Laycock <sutto@sutto.net>
2010-09-19 19:49:00 +0800

    Add in empty spec

---
+++ spec/models/dynamic_template_spec.rb
@@ -0,0 +1,11 @@
+require 'spec/spec_helper'
+
+describe DynamicTemplate do
+  
+  context 'validation'
+  
+  context 'getting the content types for select'
+  
+  context 'rendering a part'
+  
+end
