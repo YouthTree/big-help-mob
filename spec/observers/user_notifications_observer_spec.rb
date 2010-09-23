e0bdd368ec04165e6340698a349bdb332d8a7fdd
Darcy Laycock <sutto@sutto.net>
2010-09-24 00:25:22 +0800

    WIP specs for the current observer, needs to be refactored out slightly and made to actually work

---
+++ spec/observers/user_notifications_observer_spec.rb
@@ -0,0 +1,17 @@
+require 'spec/spec_helper'
+
+describe UserNotificationsObserver do
+  
+  it 'should call notify after creating a user' do
+    user = User.make
+    mock(user).notify(:signup)
+    user.save!
+  end
+  
+  it 'should not call notify after updating a user' do
+    user = User.make!
+    dont_allow(user).notify(:signup)
+    user.update_attributes :email => 'test-2@example.com'
+  end
+
+end
